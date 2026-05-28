3854d199c226e98add2db5e3c39b0ff891bcf76f
Your Name <kundan.23b0151057@abes.ac.in>
2026-05-28 17:49:47 +0530

    Initial commit

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card1 from './components/card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <Card1 />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card1 = () => {
+  return (
+        <div><div className='card'>
+      <h1>kundan kumar</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corporis eius itaque laboriosam optio officiis, blanditiis aut esse accusantium qui illo, rerum repellat, at nemo doloribus est? Non, aliquam omnis!</p> 
+    </div>
+       
+       </div>
+  )
+}
+
+export default Card1
